8a4dcc53cc3a71a44a67818e153830989d871208
napnip123-debug <napnip123@gmail.com>
2026-01-31 20:07:31 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,11 @@
 const CACHE_NAME = 'synapse-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html', './manifest.json'])));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) return response;
-        return fetch(event.request);
-      })
-  );
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
